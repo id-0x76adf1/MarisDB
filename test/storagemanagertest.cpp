b0b56ad487dd2e5cddb7418d5da39fa8feaec7a8
identifer <sorrow722@live.com>
2018-08-16 23:02:26 +0800

    Add unit test for StorageManager::open

--- test/storagemanagertest.cpp
+++ test/storagemanagertest.cpp
@@ -5,11 +5,26 @@
 using namespace MarisDB;
 namespace fs = std::filesystem;
 
+static fs::path tempFilePath()
+{
+    return fs::temp_directory_path() / std::tmpnam(nullptr);
+}
+
 TEST_CASE("Create paged file", "[StorageManager]")
 {
     StorageManager manager;
-    auto path = fs::temp_directory_path() / std::tmpnam(nullptr);
+    auto path = tempFilePath();
     manager.create(path);
 
     REQUIRE(fs::exists(path));
+}
+
+TEST_CASE("Open paged file", "[StorageManager]")
+{
+    StorageManager manager;
+    auto path = tempFilePath();
+    auto pageFile = manager.create(path);
+    manager.close(pageFile);
+
+    REQUIRE(manager.open(path));
 }
